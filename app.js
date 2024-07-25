86ef122708917fb8ac0ffc9f8af0c5e8161f73ac
Bryan <bryan.alessandromc.6061@gmail.com>
2024-07-25 18:36:34 -0400

    Add grid creation with size limit and error handling
    - Implemented grid creation with a maximum size limit of 64.
    - Added functionality to clear existing grid elements before creating a new one.
    - Included validation to ensure user input is a number and within the allowed size range.
    - Added error handling to alert if the size exceeds the limit.
    
    #Next one: styles and rainbow mode

---
+++ app.js
@@ -0,0 +1,43 @@
+const sizeBtn = document.getElementById('sizeBtn'); // button that change the size
+const gridContainer = document.getElementById('gridContainer'); // where the grids gonna be
+
+let size // Higher scope variable
+
+function createGrids() {
+    if (size <= 64) { // conditional statement to avoid a value higher than 64
+        // Clear existing items
+        while (gridContainer.firstChild) {
+            gridContainer.removeChild(gridContainer.firstChild); // remove existing grid to run another one
+        }
+    
+        // Create new grid items
+        for (let i = 0; i < size; i++) { // create the rows 
+            const rows = document.createElement('div');
+            rows.classList.add('rows');
+            gridContainer.appendChild(rows);
+            console.log(`${size} is an int number`);
+    
+            for (let j = 0; j < size; j++) { // create the columns
+                const cols = document.createElement('div');
+                cols.classList.add('cols');
+                rows.appendChild(cols);
+            }
+        }
+    } else {
+        alert('Error: Size exceeds the limit.'); // error alert if the value is higher than 64
+    }
+    
+
+}
+
+sizeBtn.addEventListener('click', function() { // event to display the prompt in the page
+    size = parseInt(prompt('Set the size that you wish', 4));
+    
+    if (Number.isNaN(size) == true) { // check if the value of the prompt 'size' isn't a number and print an error
+        alert(`The input isn't a number`);
+        console.error(`${size} isn't a int number`);
+    }
+    else {
+        createGrids(); // else the value is a number run the rest of the code
+    }
+})
