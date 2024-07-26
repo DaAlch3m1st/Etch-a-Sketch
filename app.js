eabdcca5e4a745920a764984a25859366886729b
Bryan <bryan.alessandromc.6061@gmail.com>
2024-07-25 23:25:59 -0400

    Enhance grid functionality with hover effect and refactor size change logic
    - Added hover effect to grid cells to change background color on mouseover.
    - Refactored size change logic into a separate `sizeChange` function.
    - Included error handling for input validation to ensure size is within limits and is a number.
    
    Update CSS styles for Etch-a-Sketch
    
    - Reset margin, padding, and box-sizing for all elements.
    - Set background color for the body.
    - Centered and styled `.hero` and `.main` containers.
    - Adjusted button size for `.size-btn`.
    - Styled `.grid-container` with border, border-radius, box-shadow, and background color.
    - Set display properties for `.rows` and `.cols` classes to ensure grid layout.
    
    #Next one: do a rainbow button mode

--- app.js
+++ app.js
@@ -21,7 +21,15 @@ function createGrids() {
                 const cols = document.createElement('div');
                 cols.classList.add('cols');
                 rows.appendChild(cols);
-            }
+
+                // this event listener act like a hover property in css to avoid when the user leave the mouse in the item the style dissapear
+                cols.addEventListener('mouseover', function() {
+                    cols.style.backgroundColor = 'black';
+                });
+                cols.addEventListener('mouseleave', function () {
+                    cols.style.backgroundColor = 'black';
+                });
+            }             
         }
     } else {
         alert('Error: Size exceeds the limit.'); // error alert if the value is higher than 64
@@ -30,7 +38,7 @@ function createGrids() {
 
 }
 
-sizeBtn.addEventListener('click', function() { // event to display the prompt in the page
+function sizeChange() {    
     size = parseInt(prompt('Set the size that you wish', 4));
     
     if (Number.isNaN(size) == true) { // check if the value of the prompt 'size' isn't a number and print an error
@@ -40,4 +48,6 @@ sizeBtn.addEventListener('click', function() { // event to display the prompt in
     else {
         createGrids(); // else the value is a number run the rest of the code
     }
-})
+}
+
+sizeBtn.addEventListener('click', sizeChange)
